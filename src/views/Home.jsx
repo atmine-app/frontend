20c9716b68bfd05c23efb65eef55a4504fbe1e27
guille garcia <ggarciacardiel@gmail.com>
2023-03-28 21:24:02 +0200

    adds card components v1

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React from 'react'
+import axios from 'axios';
+import { useState, useEffect } from 'react'
+import Card from '../components/Card/Card';
 
-export default function Home() {
+export default function Properties() {
+  const [properties, setProperties] = useState(null);
+
+  const getProperties = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/"
+      );
+      setProperties(response.data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getProperties();
+  }, []);
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1>All Properties</h1>
+      {properties &&
+        properties.map((property) => {
+          return (
+          <div key={property._id}>
+            <Card property={property} />
+          </div>
+        )
+        })}
     </div>
-  )
+  );
 }
