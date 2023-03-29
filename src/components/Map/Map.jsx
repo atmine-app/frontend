47c3462c66e0cca87c63731fc1e25eb715f21982
Jorge Mendez <jorge.mendezgines@gmail.com>
2023-03-29 17:28:04 +0200

    search map WIP

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { GoogleMap, Marker } from "@react-google-maps/api";
 
-const Map = ({ center, selectedLocation, locations }) => {
+const Map = ({ locations ,center, selectedLocation}) => {
+    console.log(locations)
+  
   return (
     <GoogleMap
       mapContainerStyle={{ height: "200px", width: "100%" }}
@@ -17,6 +19,9 @@ const Map = ({ center, selectedLocation, locations }) => {
       {selectedLocation && (
         <Marker position={selectedLocation} />
       )}
+      {locations && locations.map((location, index) => (
+        <Marker key={index} position={{lat: location.coordinates.lat, lng: location.coordinates.lng}} />
+      ))}
     </GoogleMap>
   );
 };
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,10 +3,20 @@ import propertyService from '../services/propertyService'
 import { useState, useEffect } from 'react'
 import Card from '../components/Card/Card';
 import SearchBar from '../components/Search/SearchBar';
+import Map from '../components/Map/Map';
+import { googleMapsConfig } from '../googleMapsConfig';
+import { useLoadScript } from '@react-google-maps/api';
+import { useJsApiLoader } from "@react-google-maps/api";
 
 export default function Properties() {
   const [properties, setProperties] = useState(null);
   const [searchValue, setSearchValue] = useState('');
+
+const { isLoaded, loadError } = useJsApiLoader({
+  id: "google-map-script",
+  googleMapsApiKey: googleMapsConfig.apiKey,
+  libraries: googleMapsConfig.libraries,
+});
 
   const handleSearch = (value) => {
     setSearchValue(value);
@@ -25,9 +35,15 @@ export default function Properties() {
     getAllProperties()
   }, [])
 
+if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading maps";
+
   return (
     <div>
     <SearchBar handleSearchValue={handleSearch} />
+    {searchValue && <div style={{ height: "200px" }}>
+  <Map center={properties[0].coordinates} locations={properties} />
+</div>}
     <h1>All Properties</h1>
     <div className="card__container">
       {properties &&
