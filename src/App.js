5f30d58e97d08c685cb4549e4282795113e033f6
guille garcia <ggarciacardiel@gmail.com>
2023-04-01 01:40:43 +0200

    updates generic org of pages and components

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ import PropertyDetail from './views/property/PropertyDetail'
 import EditProperty from './views/property/EditProperty';
 import NewProperty from './views/property/NewProperty';
 import NewBooking from './views/booking/NewBooking';
+import BookingConfirmation from './views/booking/BookingConfirmation';
+import BookingList from './views/booking/BookingList';
+import MyProfile from './views/profile/MyProfile';
+import BookingDetail from './views/booking/BookingDetail';
 
 function App() {
   return (
@@ -23,11 +27,17 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<IsPrivate><MyProfile/></IsPrivate>} /> 
+        {/* aqui hacemos el edit and save */}
         <Route path="/private" element={<IsPrivate><PrivateView /></IsPrivate>} />
         <Route path="/register-property" element={<IsPrivate><NewProperty /></IsPrivate>} />
         <Route path='/properties/:propertyId' element={<PropertyDetail />}/>
         <Route path='/properties/:propertyId/edit' element={<EditProperty />}/>
         <Route path='/properties/:propertyId/book' element={<NewBooking />}/>
+        <Route path="/bookings" element={<IsPrivate><BookingList /></IsPrivate>} />
+        <Route path="/bookings/:bookingId" element={<IsPrivate><BookingDetail /></IsPrivate>} />
+        <Route path="/bookings/:bookingId/confirmation" element={<IsPrivate><BookingConfirmation /></IsPrivate>} />
+        {/* realmente no hace falta la de confirmation => podemos ir al detail directamente */}
         <Route path="/error" element={<ErrorPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
--- src/views/booking/NewBooking.jsx
+++ src/views/booking/NewBooking.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Calendar from '../../components/Calendar/Calendar'
 
-export default function NewBooking() {
+export default function NewBooking() { // add props of the propertyId selected
   return (
     <div>
     <h1>NewBooking</h1>
@@ -9,3 +9,6 @@ export default function NewBooking() {
     </div>
   )
 }
+
+//here we process stripe payment
+//after confirmation we send the booking to the backend and redirect the user to the booking confirmation page
