f15f442d3b7b23630d75376fea2454d1e9692b88
guille garcia <ggarciacardiel@gmail.com>
2023-04-01 01:50:45 +0200

    adds sort to all properties call

--- src/views/profile/MyProfile.jsx
+++ src/views/profile/MyProfile.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react'
 
 export default function MyProfile() {
   return (
-    <div>MyProfile</div>
+    <div>
+    <h1>MyProfile</h1>
+    <img src="https://minimaltoolkit.com/images/randomdata/male/2.jpg" alt="image" />
+    </div>
   )
 }
