8d4a810f3ca48773275a67c455846bf2530a8467
guille garcia <ggarciacardiel@gmail.com>
2023-04-01 02:11:30 +0200

    prepares newBooking for Stripe integ

--- src/views/booking/NewBooking.jsx
+++ src/views/booking/NewBooking.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
-import Calendar from '../../components/Calendar/Calendar'
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import {Link} from "react-router-dom";
+import propertyService from "../../services/propertyService";
 
-export default function NewBooking() { // add props of the propertyId selected
+export default function NewBooking(prop) { // add props of the propertyId selected
+  const { propertyId } = useParams();
+  const [property, setProperty] = useState({});
+
+  const navigate = useNavigate();
+
+  const getProperty = async () => {
+    try {
+      const response = await propertyService.getProperty(propertyId);
+      setProperty(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getProperty()
+    // eslint-disable-next-line
+  }, [propertyId])
+
   return (
-    <div>
-    <h1>NewBooking</h1>
-    <Calendar/>
+    <div className="property__card-detail">
+      <img src={property.images} alt={property.title} />
+      <div className="property__card-content">
+        <h2>{property.title}</h2>
+        <p>Host: {property.owner && property.owner.username}</p>
+        <p>Price: {property.price}</p>
+      </div>
+      <div className="card-buttons">
+        <>
+        </>
+        <button type="submit">
+          <Link to={`/properties/${property._id}/book`} 
+          //here we do the stripe integration 
+          className="nav-link">
+            Book
+          </Link>
+        </button>
+      </div>
     </div>
-  )
+);
 }
 
 //here we process stripe payment
