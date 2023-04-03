8ec0192444a21d8fa3023ac257eaa4f49e8436c1
guille garcia <ggarciacardiel@gmail.com>
2023-04-03 21:23:01 +0200

    updates content and missing populate in the backend

--- src/views/booking/BookingConfirmation.jsx
+++ src/views/booking/BookingConfirmation.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import bookingService from '../../services/bookingsServices';
+/* import './BookingConfirmation.css'; */
 
 export default function BookingConfirmation() {
+  const { bookingId } = useParams();
+  const [booking, setBooking] = useState(null);
+
+  useEffect(() => {
+    const fetchBooking = async () => {
+      try {
+        const response = await bookingService.getBooking(bookingId);
+        setBooking(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchBooking();
+  }, [bookingId]);
+
   return (
-    <div>BookingConfirmation</div>
-  )
+    <div className="booking-confirmation">
+      <h2>Booking Confirmation</h2>
+      {booking ? (
+        <div className="booking-confirmation__content">
+          <p>
+            <span className="bold">Booking ID:</span> {booking._id}
+          </p>
+          <p>
+            <span className="bold">Property:</span> {booking.property?.title}
+          </p>
+          <p>
+            <span className="bold">Host:</span> {booking.property?.owner?.username}
+          </p>
+          <p>
+            <span className="bold">Renter:</span> {booking.renter?.username}
+          </p>
+          <p>
+            <span className="bold">Start Date:</span> {booking.startDate}
+          </p>
+          <p>
+            <span className="bold">End Date:</span> {booking.endDate}
+          </p>
+          <p>
+            <span className="bold">Total Price:</span> {booking.totalPrice}€
+          </p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
