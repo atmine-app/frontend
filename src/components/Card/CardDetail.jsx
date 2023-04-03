b9502fead01d5cd1e5bce73aa6fc48879990421a
Jorge Mendez <jorge.mendezgines@gmail.com>
2023-04-03 21:19:52 +0200

    cleanup consolelogs formatting

--- src/components/Card/CardDetail.jsx
+++ src/components/Card/CardDetail.jsx
@@ -1,65 +1,59 @@
-import React from 'react';
-import {Link} from "react-router-dom";
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./Card.css";
 import propertyService from "../../services/propertyService";
 
-
-export default function CardDetail({property,propertyId,rating}) {
- /*  const isOwner = x.owner && currentUser && x.owner._id === currentUser._id; */
+export default function CardDetail({ property, propertyId, rating }) {
+  /*  const isOwner = x.owner && currentUser && x.owner._id === currentUser._id; */
   const navigate = useNavigate();
   const handleDelete = async (propertyId) => {
     try {
-      await propertyService.deleteProperty(propertyId)
-      navigate('/');
+      await propertyService.deleteProperty(propertyId);
+      navigate("/");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
-      <div className="property__card-detail">
-        <img src={property.images} alt={property.title} />
-        <div className="property__card-content">
-          <h2>{property.title}</h2>
-          <p>Host: {property.owner && property.owner.username}</p>
-          <p>Catergory: {property.category}</p>
-          <p>Description: {property.description}</p>
-          <p>Price: {property.price}</p>
-          <p>Size: {property.size}</p>
-          <p>Address: {property.address}</p>
-          <p>City: {property.city}</p>
-          <p>Rating: {rating && rating && rating.averageRating} </p>
-        </div>
-        <div className='ratingsContainer'>
-          <p>Location: {rating && rating && rating.location}</p>
-          <p>Cleanliness: {rating && rating && rating.cleanliness}</p>
-          <p>Communication: {rating && rating && rating.communication}</p>
-          <p>Value: {rating && rating && rating.valueForMoney}</p>
-          <p>Amenities: {rating && rating && rating.amenities}</p>
-
-          
-        </div>
-        <div className="card-buttons">
-          <>
-            <button type="submit">
-              <Link
-                to={`/properties/${property._id}/edit`}
-                className="nav-link"
-              >
-                Edit
-              </Link>
-            </button>
-            <button type="submit" onClick={() => handleDelete(propertyId)}>
-              Delete
-            </button>
-          </>
+    <div className="property__card-detail">
+      <img src={property.images} alt={property.title} />
+      <div className="property__card-content">
+        <h2>{property.title}</h2>
+        <p>Host: {property.owner && property.owner.username}</p>
+        <p>Catergory: {property.category}</p>
+        <p>Description: {property.description}</p>
+        <p>Price: {property.price}</p>
+        <p>Size: {property.size}</p>
+        <p>Address: {property.address}</p>
+        <p>City: {property.city}</p>
+        <p>Rating: {rating && rating && rating.averageRating} </p>
+      </div>
+      <div className="ratingsContainer">
+        <p>Location: {rating && rating && rating.location}</p>
+        <p>Cleanliness: {rating && rating && rating.cleanliness}</p>
+        <p>Communication: {rating && rating && rating.communication}</p>
+        <p>Value: {rating && rating && rating.valueForMoney}</p>
+        <p>Amenities: {rating && rating && rating.amenities}</p>
+      </div>
+      <div className="card-buttons">
+        <>
           <button type="submit">
-            <Link to={`/properties/${property._id}/book`} className="nav-link">
-              Book
+            <Link to={`/properties/${property._id}/edit`} className="nav-link">
+              Edit
             </Link>
           </button>
-        </div>
+          <button type="submit" onClick={() => handleDelete(propertyId)}>
+            Delete
+          </button>
+        </>
+        <button type="submit">
+          <Link to={`/properties/${property._id}/book`} className="nav-link">
+            Book
+          </Link>
+        </button>
       </div>
+    </div>
   );
 }
