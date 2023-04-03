d3c5fb20e12ae708c7efa087135938517d1c1b6a
guille garcia <ggarciacardiel@gmail.com>
2023-04-03 20:17:25 +0200

    creates payment.js component to process payment

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -14,39 +14,37 @@ const CheckoutForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
     setLoading(true);
-  
-    try {
-      const { error, paymentMethod } = await stripe.createPaymentMethod({
-        type: "card",
-        card: elements.getElement(CardElement),
-      });
-  
-      if (error) {
-        throw new Error(error.message);
-      }
-  
+
+    if (!error) {
+      // console.log(paymentMethod)
       const { id } = paymentMethod;
-      const { data } = await axios.post(
-        "http://localhost:3000/api/checkout",
-        {
-          id,
-          amount: 10000, //cents
-        }
-      );
-  
-      if (!data.success) {
-        throw new Error("Payment failed");
+      try {
+        const { data } = await axios.post(
+          "http://localhost:8080/api/checkout",
+          {
+            id,
+            amount: 10000, //cents
+          }
+        );
+        console.log(data);
+
+        elements.getElement(CardElement).clear();
+
+        // Redirect to confirmation page
+      if (data.success) {
+        window.location.href = "/";
       }
-  
-      // Redirect to confirmation page
-      window.location.href = "/confirmation";
-    } catch (error) {
-      console.log(error.message);
-      // Display error message to user
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
     }
-  
-    setLoading(false);
   };
 
   console.log(!stripe || loading);
@@ -58,17 +56,16 @@ const CheckoutForm = () => {
         src="https://www.corsair.com/medias/sys_master/images/images/h80/hdd/9029904465950/-CH-9109011-ES-Gallery-K70-RGB-MK2-01.png"
         alt="Corsair Gaming Keyboard RGB"
         className="img-fluid"
-        width={'200px'}
       />
 
       <h3 className="text-center my-2">Price: 100$</h3>
 
       {/* cardInput from Stripe */}
-      <div className="form-group">
+      <div>
         <CardElement />
       </div>
 
-      <button disabled={!stripe} className="btn btn-success">
+      <button disabled={!stripe}>
         {loading ? (
           <div className="spinner-border text-light" role="status">
             <span className="sr-only"></span>
@@ -84,9 +81,9 @@ const CheckoutForm = () => {
 function Payment() {
   return (
     <Elements stripe={stripePromise}>
-      <div className="container p-4">
-        <div className="row h-100">
-          <div className="col-md-4 offset-md-4 h-100">
+      <div>
+        <div>
+          <div>
             <CheckoutForm />
           </div>
         </div>
