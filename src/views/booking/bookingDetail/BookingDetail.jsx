67702abf230d77ef33e4d700e391a8de1bfad126
guille garcia <ggarciacardiel@gmail.com>
2023-04-05 19:23:23 +0200

    adds swipper and styles v1

--- src/views/booking/bookingDetail/BookingDetail.jsx
+++ src/views/booking/bookingDetail/BookingDetail.jsx
@@ -1,10 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import bookingService from '../../../services/bookingsServices'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import bookingService from "../../../services/bookingsServices";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
+import "./BookingDetail.css";
 
 export default function BookingDetail() {
-  const { bookingId } = useParams()
-  const [booking, setBooking] = useState({})
+  const { bookingId } = useParams();
+  const [booking, setBooking] = useState({});
 
   const getBooking = async () => {
     try {
@@ -20,38 +25,57 @@ export default function BookingDetail() {
     // eslint-disable-next-line
   }, [bookingId]);
 
-
   if (!booking) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   return (
     <div>
       <h2>Booking Details</h2>
       <div className="booking-list__item-property">
-        <img
-          src={booking.property?.images?.[0]}
-          alt={booking.property?.title}
-        />
-        <div className="booking-list__item-property-details">
-          <h3>{booking.property?.title}</h3>
-          <p>
-            <span className="bold">Booking ID:</span> {booking._id}
-          </p>
-          <p>
-            <span className="bold">Renter:</span> {booking.renter?.username}
-          </p>
-          <p>
-            <span className="bold">Start Date:</span> {booking.startDate}
-          </p>
-          <p>
-            <span className="bold">End Date:</span> {booking.endDate}
-          </p>
-          <p>
-            <span className="bold">Total Price:</span> {booking.totalPrice}€
-          </p>
+        <Swiper
+          className=" ImageContainer mySwiper"
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+        >
+          {booking.property?.images?.length > 0 &&
+            booking.property.images.map((image, index) => (
+              <SwiperSlide key={index}>
+                <img
+                  src={image}
+                  alt={`${booking.property.title} - ${index + 1}`}
+                />
+              </SwiperSlide>
+            ))}
+        </Swiper>
+        <div class="booking-confirmation">
+          <div class="booking-confirmation__details">
+            <div class="booking-confirmation__property-details">
+              <h3>{booking.property?.title}</h3>
+              <div class="booking-confirmation__property-info">
+                <p>
+                  <span class="bold">Booking ID:</span> {booking._id}
+                </p>
+                <p>
+                  <span class="bold">Renter:</span> {booking.renter?.username}
+                </p>
+                <p>
+                  <span class="bold">Start Date:</span> {booking.startDate}
+                </p>
+                <p>
+                  <span class="bold">End Date:</span> {booking.endDate}
+                </p>
+                <p>
+                  <span class="bold">Total Price:</span> {booking.totalPrice} €
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
