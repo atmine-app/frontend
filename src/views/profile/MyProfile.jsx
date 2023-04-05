fe7f90ad86fe197edf056a1b08c1a3a9980043dd
guille garcia <ggarciacardiel@gmail.com>
2023-04-05 20:55:31 +0200

    fixes upload and save, backend wip

--- src/views/profile/MyProfile.jsx
+++ src/views/profile/MyProfile.jsx
@@ -1,28 +1,53 @@
-import React, { useContext } from 'react';
-import {AuthContext} from '../../context/AuthContext'
+import React, { useContext, useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 import { useAuth } from '../../hooks/useAuth';
-
+import userService from '../../services/userService';
+import Multiupload from '../../components/Multiupload/Multiupload';
 
 export default function MyProfile() {
-
-  const { isLoggedIn, logOutUser } = useContext(AuthContext); 
+  const { isLoggedIn, logOutUser } = useContext(AuthContext);
   const { user } = useAuth();
+  const [avatarImage, setAvatarImage] = useState('');
+
+   console.log(user);
+
+  const handleAvatarUpload = (imageData) => {
+    setAvatarImage(imageData.array[0]);
+  };
+
+  const handleSaveAvatar = async () => {
+    try {
+      await userService.updateUser({ ...user, avatar: avatarImage });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div>
       <h1>MyProfile</h1>
       {user && (
         <div>
+          {user.avatar && (
+            <img src={user.avatar} alt="Avatar" style={{ width: '100px', height: '100px' }} />
+          )}
           <p>Email: {user.email}</p>
-          <p>Name: {user.name}</p>
-          <p>Lastname: {user.lastname}</p>
-          <p>Phone: {user.phone}</p>
+          <p>Name: {user.username}</p>
           <p>Google ID: {user.googleId}</p>
-          <p>Birthday: {user.birthdate}</p>
-          <p>Passport/ID Number: {user.passport}</p>
-        {isLoggedIn && <li><button onClick={() => logOutUser()}>Log out</button></li>}
+          <p>Status: {user.status}</p>
+          <p>Status: {user.role}</p>
+          {avatarImage && (
+            <img src={avatarImage} alt="Avatar" style={{ width: '100px', height: '100px' }} />
+          )}
+          {isLoggedIn && (
+            <div>
+              <Multiupload onImageDataChange={handleAvatarUpload} />
+              <button onClick={handleSaveAvatar}>Save</button>
+              <button onClick={() => logOutUser()}>Log out</button>
+            </div>
+          )}
         </div>
       )}
     </div>
-  )
+  );
 }
