ce80c94553e8535aac7ba147c5748810a1be0626
guille garcia <ggarciacardiel@gmail.com>
2023-04-15 10:02:10 +0200

    wip user inactivation

--- src/views/profile/EditProfile.jsx
+++ src/views/profile/EditProfile.jsx
@@ -1,9 +1,10 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import { useAuth } from "../../hooks/useAuth";
 import { toast } from "react-toastify";
 import userService from "../../services/userService";
+import propertyService from "../../services/propertyService";
 import SingleImageUpload from "../../components/SingleUpload/SingleImageUpload";
 import BackNavigationFloat from "../../components/BackNavigation/BackNavigationFloat";
 import "./MyProfile.css";
@@ -13,6 +14,7 @@ export default function EditProfile() {
   const { user } = useAuth();
   const [avatarImage, setAvatarImage] = useState("");
   const [showAvatarUpload, setShowAvatarUpload] = useState(false);
+  const [hasActiveBookings, setHasActiveBookings] = useState(false);
   const navigate = useNavigate();
 
   const handleAvatarUpload = (imageData) => {
@@ -47,15 +49,56 @@ export default function EditProfile() {
     setShowAvatarUpload(!showAvatarUpload);
   };
 
+  useEffect(() => {
+    const checkActiveBookings = async () => {
+      try {
+        const allProperties = await propertyService.getAllProperties();
+        const userProperties = allProperties.filter(property => property.owner._id === user._id);
+        const activeBookings = userProperties.some(
+          property => property?.bookings?.some(booking => booking.status === 'active')
+        );
+        console.log(allProperties)
+        console.log(userProperties)
+        console.log(activeBookings)
+        setHasActiveBookings(activeBookings);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    checkActiveBookings();
+  }, [user._id]);
+
+  const handleInactivateUser = async () => {
+    try {
+      await userService.inactivateUser(user._id);
+      logOutUser();
+      toast.success("User successfully inactivated", {
+        position: "top-right",
+        autoClose: 2500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
   return (
     <div>
       <BackNavigationFloat />
       <div className="editProfileContainer">
-          <h2 className="editProfileTitle">Edit Profile</h2>
+        <h2 className="editProfileTitle">Edit Profile</h2>
         <div className="editProfileBody">
           <div className="editProfileSection">
             <div className="avatarSection">
-            <div className="avatarImage">
+              <div className="avatarImage">
                 {(avatarImage || user.avatar) && (
                   <img
                     src={avatarImage || user.avatar}
@@ -116,6 +159,18 @@ export default function EditProfile() {
             </div>
           </div>
         </div>
+        {user.status === "active" && !hasActiveBookings && (
+          <div className="editProfileLogoutSection section">
+            <button className="cta-button danger" onClick={handleInactivateUser}>
+              Inactivate User
+            </button>
+          </div>
+        )}
+        {hasActiveBookings && (
+          <div className="editProfileLogoutSection section">
+            <p>You cannot inactivate your user while there are active bookings ahead.</p>
+          </div>
+        )}
         <div className="editProfileLogoutSection section">
           <button className="cta-button danger" onClick={handleLogout}>
             Log out
@@ -124,4 +179,4 @@ export default function EditProfile() {
       </div>
     </div>
   );
-}
+        }
