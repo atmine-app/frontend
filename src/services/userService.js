227b71eeca3d2c31b671793e96da22a9bebc4d80
Jorge Mendez <jorge.mendezgines@gmail.com>
2023-04-17 11:25:34 +0200

    chat fixed

--- src/services/userService.js
+++ src/services/userService.js
@@ -19,9 +19,15 @@ class UserService {
     return this.api.get('/me').then(({ data }) => data);
   }
   getOtherUser(otherUserId) {
+    console.log('otherUserId service', otherUserId)
     return this.api.get(`/chat/${otherUserId}`)
       .then(({ data }) => {
+        console.log('User data received:', data);
         return data;
+      })
+      .catch((error) => {
+        console.error('Error in API call:', error);
+        throw error;
       });
   }
 
