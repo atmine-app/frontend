dde28bd07543d41eba272780bd8e6cf8b0c8d60e
guille garcia <ggarciacardiel@gmail.com>
2023-04-21 17:32:06 +0200

    summary fixed

--- src/views/profile/RevenueSummaryTable.jsx
+++ src/views/profile/RevenueSummaryTable.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./RevenueSummaryTable.css";
 
 const RevenueSummaryTable = ({ properties }) => {
+  console.log("RevenueSummaryTable properties:", properties); 
   const totalRevenue = properties.reduce((total, { revenue }) => total + revenue, 0);
   const totalBookings = properties.reduce((total, { bookingCount }) => total + bookingCount, 0);
   const totalFees = totalRevenue * 0.1;
@@ -20,23 +21,23 @@ const RevenueSummaryTable = ({ properties }) => {
         {properties.map(({ property, revenue, bookingCount }) => (
           <tr key={property._id}>
             <td>{property.title}</td>
-            <td>{revenue.toFixed(2)} €</td>
+            <td>{revenue.toFixed(1)} €</td>
             <td>{bookingCount}</td>
           </tr>
         ))}
         <tr>
           <td><strong></strong></td>
-          <td><strong>{totalRevenue.toFixed(2)} €</strong></td>
+          <td><strong>{totalRevenue.toFixed(1)} €</strong></td>
           <td><strong>{totalBookings}</strong></td>
         </tr>
         <tr>
           <td style={{ color: "#e06a6a" }}><strong>Service Fees</strong></td>
-          <td style={{ color: "#e06a6a" }}><strong>- {totalFees.toFixed(2)} €</strong></td>
+          <td style={{ color: "#e06a6a" }}><strong>- {totalFees.toFixed(1)} €</strong></td>
           <td></td>
         </tr>
         <tr>
-          <td><strong>Total Revenue</strong></td>
-          <td><strong>{netRevenue.toFixed(2)} €</strong></td>
+          <td style={{ color: "#06b782" }}><strong>Total Revenue</strong></td>
+          <td style={{ color: "#06b782" }}><strong>{netRevenue.toFixed(1)} €</strong></td>
           <td></td>
         </tr>
       </tbody>
